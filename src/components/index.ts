ba15f8e1c2ded718ebaeaa1c5a32e7925cbeb7e0
GustavoPasquiniLucas <gupalu2016@gmail.com>
2026-04-30 22:34:34 -0300

    aula 30/04

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,7 @@
 /**
  * Exportação centralizada dos componentes
- * 
- * Permite importar assim:
- * import { ConsultaCard } from './src/components';
- * 
- * Em vez de:
- * import ConsultaCard from './src/components/ConsultaCard';
  */
 
 export { default as ConsultaCard } from "./ConsultaCard";
+export { default as Loading } from "./Loading";
+export { default as EmptyState } from "./EmptyState";
